b5bb092c7294a0956389510d76f3ba588033d5a8
James Carbine <jamescarbine@gmail.com>
2019-01-28 19:00:01 -0700

    Fix regex

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,7 @@
 bash 'update_nginx_passenger_conf' do
   cwd '/etc/nginx/'
   code <<-CONF
-    total_mem_kib=$(sed -nr '/^MemTotal:/s/.*\b([0-9]+).*/\1/p' /proc/meminfo)
+    total_mem_kib=$(sed -nr '/^MemTotal:/s/.*\\b([0-9]+).*/\\1/p' /proc/meminfo)
     proc_size_kib=$(( 250 * 1024 ))
     proc_count=$(( ( total_mem_kib + proc_size_kib / 2 ) / proc_size_kib ))
     min_inst=$(( proc_count - 1 ))
