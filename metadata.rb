a8162ec77f4734bf03f2be2dfb9f6ae5e98da599
James Carbine <jamescarbine@gmail.com>
2019-01-28 18:10:12 -0700

    support chef >= 12

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures passenger_config'
 long_description 'Installs/Configures passenger_config'
 version '0.1.0'
-chef_version '>= 13.0'
+chef_version '>= 12.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
